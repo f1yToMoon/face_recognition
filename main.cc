d62f6c1645adc52cd5a1f0e1e92d381e39773165
f1yToMoon <90560141+f1yToMoon@users.noreply.github.com>
2022-09-12 23:25:19 +0300

    Update main.cc

--- main.cc
+++ main.cc
@@ -2,29 +2,32 @@
 #include <iostream>
 
 #include <string>
-
+#include <vector>
 
 int main() {
-    std::vector<int> vec, test;
-    std::vector<std::vector<int>> f, k;
+    std::vector<int> vec;
+    std::vector<std::vector<int>> f;
     for (int i = 0; i < 100; ++i) {
         vec.push_back(std::rand() % 10304);
     }
+
     for (int i = 0; i < 40; ++i) {
         cv::Mat mat = cv::imread("/home/nick/images/archive/s" + std::to_string(i) + "/1.pgm", cv::IMREAD_GRAYSCALE);
         mat.convertTo(mat, CV_32F);
         std::vector<int> img((float *) mat.data, (float *) mat.data + mat.rows * mat.cols);
 
         std::vector<int> v;
-        for (int i = 0; i < 100; ++i) {
-            v.push_back(img[vec[i]]);
+        for (int j = 0; j < 100; ++j) {
+            v.push_back(img[vec[j]]);
         }
 
         f.push_back(v);
     }
+
+    int k = 0;
     for (int h = 0; h < 40; ++h) {
-        std::cout << h << ") ";
-        int l = 0;
+        //std::cout << h << ") ";
+        float l = 0;
         for (int u = 2; u < 11; ++u) {
             std::vector<int> x;
             cv::Mat mat = cv::imread(
@@ -33,6 +36,7 @@ int main() {
             mat.convertTo(mat, CV_32F);
             std::vector<int> img((float *) mat.data, (float *) mat.data + mat.rows * mat.cols);
 
+            std::vector<int> test;
             for (int i = 0; i < 100; ++i) {
                 test.push_back(img[vec[i]]);
             }
@@ -47,14 +51,16 @@ int main() {
 
             auto min = *std::min_element(x.begin(), x.end());
             for (int i = 0; i < 40; ++i) {
-                if (x[i] == min && i == h) {
+                if (x[i] == min && h == i) {
                     l += 1;
                 }
             }
 
         }
-        std::cout << l << "\n";
+        //std::cout << l << "\n";
+        k += l;
     }
+    std::cout << k;
     return 0;
 }
 
