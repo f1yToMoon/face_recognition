31944786d3856c84cde924c102715a89e5b9653d
f1yToMoon <90560141+f1yToMoon@users.noreply.github.com>
2022-09-19 23:02:20 +0300

    Update main.cc
    +uniform distribution
    -rand

--- main.cc
+++ main.cc
@@ -1,14 +1,23 @@
+#include <algorithm>
 #include <opencv2/opencv.hpp>
 #include <iostream>
-
 #include <string>
 #include <vector>
+#include <random>
+
+template<typename T>
+T random(T range_from, T range_to) {
+    std::random_device rand_dev;
+    std::mt19937 generator(rand_dev());
+    std::uniform_int_distribution<T> distr(range_from, range_to);
+    return distr(generator);
+}
 
 int main() {
     std::vector<int> vec;
     std::vector<std::vector<int>> f;
-    for (int i = 0; i < 100; ++i) {
-        vec.push_back(std::rand() % 10304);
+    for (int i = 0; i < 1000; ++i) {
+        vec.push_back(random(1, 10304));
     }
 
     for (int i = 0; i < 40; ++i) {
@@ -17,7 +26,7 @@ int main() {
         std::vector<int> img((float *) mat.data, (float *) mat.data + mat.rows * mat.cols);
 
         std::vector<int> v;
-        for (int j = 0; j < 100; ++j) {
+        for (int j = 0; j < 1000; ++j) {
             v.push_back(img[vec[j]]);
         }
 
@@ -26,7 +35,7 @@ int main() {
 
     int k = 0;
     for (int h = 0; h < 40; ++h) {
-        //std::cout << h << ") ";
+        std::cout << h << ") ";
         float l = 0;
         for (int u = 2; u < 11; ++u) {
             std::vector<int> x;
@@ -37,13 +46,13 @@ int main() {
             std::vector<int> img((float *) mat.data, (float *) mat.data + mat.rows * mat.cols);
 
             std::vector<int> test;
-            for (int i = 0; i < 100; ++i) {
+            for (int i = 0; i < 1000; ++i) {
                 test.push_back(img[vec[i]]);
             }
 
             for (int i = 0; i < 40; ++i) {
                 int diff = 0;
-                for (int j = 0; j < 100; ++j) {
+                for (int j = 0; j < 1000; ++j) {
                     diff += abs(f[i][j] - test[j]);
                 }
                 x.push_back(diff);
@@ -57,10 +66,10 @@ int main() {
             }
 
         }
-        //std::cout << l << "\n";
+        std::cout << l << "\n";
         k += l;
     }
-    std::cout << k;
+    std::cout << k << "/360";
     return 0;
 }
 
