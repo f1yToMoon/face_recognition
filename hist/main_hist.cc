a279100974c7fdbd52a788aeda98f7f3504ba132
f1yToMoon <90560141+f1yToMoon@users.noreply.github.com>
2022-11-16 18:43:39 +0300

    Update main_hist.cc

--- hist/main_hist.cc
+++ hist/main_hist.cc
@@ -6,16 +6,27 @@
 #include "opencv2/imgcodecs.hpp"
 
 int main() {
+    int bin = 8;
     std::vector<std::vector<int>> f;
     for (int i = 0; i < 40; ++i) {
         std::vector<int> hist(256);
+        std::vector<int> gg;
         cv::Mat mat = cv::imread("/home/nick/images/archive/s" + std::to_string(i) + "/1.pgm", cv::IMREAD_GRAYSCALE);
         mat.convertTo(mat, CV_32F);
         std::vector<int> img((float *) mat.data, (float *) mat.data + mat.rows * mat.cols);
         for(int j : img) {
             hist[j]++;
         }
-        f.push_back(hist);
+        for (int iii = 1 ; iii <= bin; ++iii) {
+            int ks = (iii-1)*256/bin;
+            int kf = (iii)*256/bin - 1;
+            int sum = 0;
+            for (int aaa = ks; aaa <= kf; ++aaa) {
+                sum += hist[aaa];
+            }
+            gg.push_back(sum);
+        }
+        f.push_back(gg);
     }
 
     int k = 0;
@@ -23,14 +34,24 @@ int main() {
         std::cout << h << ") ";
         float l = 0;
         for (int u = 2; u < 11; ++u) {
-            std::vector<int> x, test(256);
+            std::vector<int> x, gg(256);
+            std::vector<int> test;
             cv::Mat mat = cv::imread(
                     "/home/nick/images/archive/s" + std::to_string(h) + "/" + std::to_string(u) + ".pgm",
                     cv::IMREAD_GRAYSCALE);
             mat.convertTo(mat, CV_32F);
             std::vector<int> img((float *) mat.data, (float *) mat.data + mat.rows * mat.cols);
             for(int i : img) {
-                test[i]++;
+                gg[i]++;
+            }
+            for (int iii = 1 ; iii <= bin; ++iii) {
+                int ks = (iii-1)*256/bin;
+                int kf = (iii)*256/bin - 1;
+                int sum = 0;
+                for (int aaa = ks; aaa <= kf; ++aaa) {
+                    sum += gg[aaa];
+                }
+                test.push_back(sum);
             }
 
             for (int i = 0; i < 40; ++i) {
